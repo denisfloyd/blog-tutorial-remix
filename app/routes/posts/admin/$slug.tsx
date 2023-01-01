f8583fad72e1ecb4ac5590f86c74fdcef593022a
Denis Ladeira <105209483+denisfloyd@users.noreply.github.com>
2023-01-01 15:20:57 -0300

    feat: :sparkles: post admin update page

---
+++ app/routes/posts/admin/$slug.tsx
@@ -0,0 +1,118 @@
+import type { ActionArgs, LoaderArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useTransition,
+} from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+import { getPost, updatePost } from "~/models/post.server";
+
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData();
+
+  const title = formData.get("title");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  const errors = {
+    title: title ? null : "Title is required",
+    slug: slug ? null : "Slug is required",
+    markdown: markdown ? null : "Markdown is required",
+  };
+  const hasErrors = Object.values(errors).some((errorMessage) => errorMessage);
+  if (hasErrors) {
+    return json(errors);
+  }
+
+  invariant(typeof title === "string", "title must be a string");
+  invariant(typeof slug === "string", "slug must be a string");
+  invariant(typeof markdown === "string", "markdown must be a string");
+
+  await new Promise((res) => setTimeout(res, 1000));
+
+  await updatePost({ title, slug, markdown });
+
+  return redirect("/posts/admin");
+};
+
+export const loader = async ({ params }: LoaderArgs) => {
+  invariant(params.slug, `params.slug is required`);
+
+  const post = await getPost(String(params.slug));
+  invariant(post, `Post not found: ${params.slug}`);
+
+  return json({ post });
+};
+
+const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
+
+export default function NewPost() {
+  const { post } = useLoaderData<typeof loader>();
+  const errors = useActionData<typeof action>();
+
+  const transition = useTransition();
+  const isCreating = Boolean(transition.submission);
+
+  return (
+    <Form method="put">
+      <p>
+        <label>
+          Post Slug:{" "}
+          {errors?.slug ? (
+            <em className="text-red-600">{errors.slug}</em>
+          ) : null}
+          <input
+            type="text"
+            name="slug"
+            className={inputClassName}
+            defaultValue={post.slug}
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          Post Title:{" "}
+          {errors?.title ? (
+            <em className="text-red-600">{errors.title}</em>
+          ) : null}
+          <input
+            type="text"
+            name="title"
+            className={inputClassName}
+            defaultValue={post.title}
+          />
+        </label>
+      </p>
+
+      <p>
+        <label>
+          Markdown:{" "}
+          {errors?.markdown ? (
+            <em className="text-red-600">{errors.markdown}</em>
+          ) : null}
+        </label>
+        <br />
+        <textarea
+          id="markdown"
+          rows={20}
+          name="markdown"
+          className={`${inputClassName} font-mono`}
+        >
+          {post.markdown}
+        </textarea>
+      </p>
+      <p className="text-right">
+        <button
+          type="submit"
+          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          disabled={isCreating}
+        >
+          {isCreating ? "Creating..." : "Update Post"}
+        </button>
+      </p>
+    </Form>
+  );
+}
