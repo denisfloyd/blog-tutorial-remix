3a25385009cf8436eff03386a431f207879310f4
Denis Ladeira <105209483+denisfloyd@users.noreply.github.com>
2023-01-22 10:53:41 -0300

    feat: :recycle: create the edit page

--- app/routes/posts/admin/$slug.tsx
+++ app/routes/posts/admin/$slug.tsx
@@ -1,30 +1,44 @@
-import type { ActionArgs, LoaderArgs } from "@remix-run/node";
+import type { ActionArgs, ActionFunction, LoaderFunction } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import {
-  Form,
-  useActionData,
-  useLoaderData,
-  useTransition,
-} from "@remix-run/react";
+import { Form, useActionData, useTransition } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
-import { getPost, updatePost } from "~/models/post.server";
+import { createPost } from "~/models/post.server";
+import { requireAdminUser } from "~/session.server";
 
-export const action = async ({ request }: ActionArgs) => {
+export const loader: LoaderFunction = async ({ request, params }) => {
+  await requireAdminUser(request);
+  if (params.slug === "new") {
+    return json({});
+  }
+  return json({ post: null });
+};
+
+type ActionData =
+  | {
+      title: null | string;
+      slug: null | string;
+      markdown: null | string;
+    }
+  | undefined;
+
+export const action: ActionFunction = async ({ request, params }: ActionArgs) => {
+  await requireAdminUser(request);
   const formData = await request.formData();
 
   const title = formData.get("title");
   const slug = formData.get("slug");
   const markdown = formData.get("markdown");
 
-  const errors = {
+  const errors: ActionData = {
     title: title ? null : "Title is required",
     slug: slug ? null : "Slug is required",
     markdown: markdown ? null : "Markdown is required",
   };
+
   const hasErrors = Object.values(errors).some((errorMessage) => errorMessage);
   if (hasErrors) {
-    return json(errors);
+    return json<ActionData>(errors);
   }
 
   invariant(typeof title === "string", "title must be a string");
@@ -33,76 +47,57 @@ export const action = async ({ request }: ActionArgs) => {
 
   await new Promise((res) => setTimeout(res, 1000));
 
-  await updatePost({ title, slug, markdown });
+  if (params.slug === "new") {
+    await createPost({ title, slug, markdown });
+  } else {
+    // TODO update post
+  }
 
   return redirect("/posts/admin");
 };
 
-export const loader = async ({ params }: LoaderArgs) => {
-  invariant(params.slug, `params.slug is required`);
-
-  const post = await getPost(String(params.slug));
-  invariant(post, `Post not found: ${params.slug}`);
-
-  return json({ post });
-};
-
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
-export default function EditPost() {
-  const { post } = useLoaderData<typeof loader>();
-  const errors = useActionData<typeof action>();
+export default function NewPost() {
+  const errors = useActionData() as ActionData;
 
   const transition = useTransition();
   const isCreating = Boolean(transition.submission);
 
   return (
-    <Form method="put">
-      <p>
-        <label>
-          Post Slug:{" "}
-          {errors?.slug ? (
-            <em className="text-red-600">{errors.slug}</em>
-          ) : null}
-          <input
-            type="text"
-            name="slug"
-            className={inputClassName}
-            defaultValue={post.slug}
-          />
-        </label>
-      </p>
+    <Form method="post">
       <p>
         <label>
           Post Title:{" "}
           {errors?.title ? (
             <em className="text-red-600">{errors.title}</em>
           ) : null}
-          <input
-            type="text"
-            name="title"
-            className={inputClassName}
-            defaultValue={post.title}
-          />
+          <input type="text" name="title" className={inputClassName} />
         </label>
       </p>
-
       <p>
         <label>
+          Post Slug:{" "}
+          {errors?.slug ? (
+            <em className="text-red-600">{errors.slug}</em>
+          ) : null}
+          <input type="text" name="slug" className={inputClassName} />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="markdown">
           Markdown:{" "}
           {errors?.markdown ? (
             <em className="text-red-600">{errors.markdown}</em>
           ) : null}
-          <input
-            // id="markdown"
-            // rows={20}
-            type="text"
-            name="markdown"
-            className={`${inputClassName} font-mono`}
-            defaultValue={post.markdown}
-          />
         </label>
         <br />
+        <textarea
+          id="markdown"
+          rows={20}
+          name="markdown"
+          className={`${inputClassName} font-mono`}
+        />
       </p>
       <p className="text-right">
         <button
@@ -110,7 +105,7 @@ export default function EditPost() {
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
           disabled={isCreating}
         >
-          {isCreating ? "Creating..." : "Update Post"}
+          {isCreating ? "Creating..." : "Create Post"}
         </button>
       </p>
     </Form>
